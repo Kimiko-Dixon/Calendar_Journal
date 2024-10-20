132e4218a624cc9c39c777ef308bcba0fc31dbab
TRASHpanda364 <dixonkimiko@gmail.com>
2024-10-20 14:36:04 -0400

    added page filler

---
+++ client/src/pages/Journal.jsx
@@ -0,0 +1,7 @@
+const Journal = ()=>{
+    return (
+        <h1>Journal Page</h1>
+    )
+}
+
+export default Journal
---
+++ client/src/pages/LoginSignupPage.jsx
@@ -0,0 +1,7 @@
+const SignupLoginPage = ()=>{
+    return (
+        <h1>SignupLoginPage Page</h1>
+    )
+}
+
+export default SignupLoginPage
